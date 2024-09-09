66e6251a63aa058b50b3936bb3b7bf66648b6239
deepanisanthan <149642329+deepanisanthan@users.noreply.github.com>
2024-09-09 16:46:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function submitForm() {
     const xhr = new XMLHttpRequest();
 
     // Set up request
-    xhr.open('POST', 'API_INVOKE_URL/register', true);
+    xhr.open('POST',https://y6pdsm5mk1.execute-api.us-east-1.amazonaws.com/prod, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     // Set up response handler
